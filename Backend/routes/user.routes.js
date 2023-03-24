a47bc885a73c0b8fdc507344b0f21cfcf7153f10
Govind <govind@Govinds-MacBook-Air.local>
2023-03-24 21:59:39 +0530

    route added for users

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -4,6 +4,15 @@ const userRouter = express.Router();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
+userRouter.get("/", async (req, res) => {
+  try {
+    const users = await UserModel.find();
+    res.send(users);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 // registration:
 userRouter.post("/register", async (req, res) => {
   const { email, password, location, age } = req.body;
